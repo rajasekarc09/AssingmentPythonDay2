760ed32dd8dee2076fd9ff903fdfb2c35be051b8
rajasekarc09 <45358624+rajasekarc09@users.noreply.github.com>
2020-07-22 10:49:46 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,33 @@
+#Assignment of Day 2
+
+#Variable: Syntax : Variable Name = Val
+
+
+number = 2020
+
+#Operators and Assignment operation and also formatted strings
+
+Operand1 = 15
+operand2 = 5
+operation = 0
+
+#Addition 
+operation = operand1 + operand2
+print(f"Addition Value = {operation} \n") #Escape sequence
+#Subtraction
+operation = operand1 - operand2
+print(f"Subtraction Value = {operation} \n")
+#Division
+operation = operand1 // operand2 #Return integer
+print(f"Divided Value = {operation} \n") 
+#Reminder
+operation = operand1 % operand2
+print(f'Reminder\s' = {operation} \n) #Back Slash
+#Multiplication
+operation = operand1 * operand2
+print(f"Multiplied Value = {operation} \n") 
+operation = operand2 ** operand1
+print(f"Output = {operation} \n") 
+#Triple Quotes
+print("""Day2 Assignment
+                 Lets upgrade !!!""")
